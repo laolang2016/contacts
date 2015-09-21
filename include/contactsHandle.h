ee19efa64657cd5087e315c1a0d6b9c6a5f28935
laolang2016 <xiaodaima2016@163.com>
2015-09-21 20:06:27 +0800

    下一步： 	1、添加全局变量，保存链表 	2、以glib完成DRUD 	3、测试libxml2

---
+++ include/contactsHandle.h
@@ -0,0 +1,11 @@
+/** @brief contactsHandle.h
+ * 对联系人的保存
+ * 
+ */
+
+#ifndef INCLUDE_CONTACTSHANDLE_H_
+#define INCLUDE_CONTACTSHANDLE_H_
+
+
+
+#endif /* INCLUDE_CONTACTSHANDLE_H_ */
---
+++ include/gloab.h
@@ -0,0 +1,12 @@
+/** @brief gloab.h
+ * 全局变量
+ * 
+ */
+#ifndef INCLUDE_GLOAB_H_
+#define INCLUDE_GLOAB_H_
+
+
+
+
+
+#endif /* INCLUDE_GLOAB_H_ */
